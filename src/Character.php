475f3190298a3a52928bc09a5cb31b4a9de0f14d
Estherherrera <graxyherrera@gmail.com>
2022-07-29 22:06:35 +0200

    terminada por completo la primera iteraciÃon

--- src/Character.php
+++ src/Character.php
@@ -2,10 +2,48 @@
 
 namespace App;
 
-class Character {
+class Character
+{
 
-    
-    
+    private int $health;
+    private int $level;
+    private bool $alive;
+
+    public function __construct()
+    {
+        $this->health = 1000;
+        $this->level = 1;
+        $this->alive = true;
+    }
+    public function getHealth()
+    {
+        return $this->health;
+    }
+
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    public function getAlive()
+    {
+        if ($this->health === 0) {
+            $this->alive = false;
+        }
+        return $this->alive;
+    }
+
+    public function attack($enemy)
+    {
+        $enemy->health = $enemy->health - 100;
+    }
+
+    public function heal($enemy)
+    {
+        for ($i = 1; $i <= 10; $i = $i + 1) {
+            if ($enemy->alive && $enemy->health  < 1000) {
+                $enemy->health = $enemy->health + 100;
+            }
+        }
+    }
 }
-
-?>
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -6,12 +6,89 @@ use PHPUnit\Framework\TestCase;
 use App\Character;
 use App\Faction;
 
+use function PHPUnit\Framework\assertEquals;
+
 class CharacterTest extends TestCase {
 
 	/** @test */
-	public function test_function_example()
+	public function test_character_health_starting_at_1000_points()
 	{
-		$this->assertEquals(4, 4);
+		//Given-Dado-Escenario
+		$goku = new Character;
+
+		//When-cuándo-acción
+		$gokuHealth = $goku->getHealth();
+
+		//Then - Entonces - Después - Debería dar
+		$this->assertEquals(1000, $gokuHealth);
+	}
+
+	public function test_character_level_starting_at_1()
+	{
+		//Given-Dado-Escenario
+		$goku = new Character;
+
+		//When-cuándo-acción
+		$gokuLevel = $goku->getLevel();
+
+		//Then-Entonces-Después-Debería dar
+		$this->assertEquals(1, $gokuLevel);
+	}
+
+	public function test_character_alive_or_dead_starting_alive()
+	{
+		//Given-Dado-Escenario
+		$goku = new Character;
+
+		//When-Cuándo-Acción
+		$gokuAlive = $goku->getAlive() ;
+
+		//Then-Entonces-Después-Debería dar
+		$this->assertEquals(true, $gokuAlive);
+	}
+
+	public function test_character_takes_damage()
+	{
+		//Given
+		$freezer = new Character;
+		$vegeta = new Character;
+
+		//When
+		$freezer->attack($vegeta);
+		$result = $vegeta->getHealth();
+
+		//Then-Debería dar
+		$this->assertEquals(900, $result);
+	}
+	public function test_when_character_health_equals_0_he_died()
+	{
+		//Given
+		$freezer = new Character;
+		$vegeta = new Character;
+
+		//When
+		for ( $i = 1; $i <= 10; $i = $i + 1 ){
+		$freezer->attack($vegeta);
+		}
+		$result = $vegeta->getHealth();
+		$liveVegeta = $vegeta->getAlive();
+		//Then-Debería dar
+		$this->assertEquals(0, $result);
+		$this->assertEquals(false, $liveVegeta);
+
+	}
+	public function test_a_character_can_Heal_a_Character()
+	{
+		//Given
+		$goku = new Character;
+		$vegeta = new Character;
+
+		//When
+		$goku->heal($vegeta);
+		$healVegeta = $vegeta->getHealth();
+
+		//
+		$this->assertEquals(1000, $healVegeta);
 	}
 	
 
